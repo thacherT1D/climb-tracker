c6993e04e493843700920d43fbd7e63fa498d219
Thacher <thacher@tidepool.org>
2020-02-21 15:01:55 -0800

    added Climb component, updated linting

---
+++ src/components/Climb.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eEe;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+  @media (min-width: 500px) {
+    width: 450px;
+  }
+`;
+
+const climb = (props) => (
+  <StyledDiv>
+    <p>Climb Component</p>
+  </StyledDiv>
+);
+
+export default climb;
