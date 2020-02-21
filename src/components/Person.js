0ee9244b9e5fb722738d8a08066cf8e203c262d6
Thacher <thacher@tidepool.org>
2020-02-21 14:43:11 -0800

    set up stylelint

--- src/components/Person.js
+++ src/components/Person.js
@@ -6,11 +6,10 @@ import styled from 'styled-components';
 const StyledDiv = styled.div`
   width: 60%;
   margin: 16px auto;
-  border: 1px solid #eee;
+  border: 1px solid #eEe;
   box-shadow: 0 2px 3px #ccc;
   padding: 16px;
   text-align: center;
-
   @media (min-width: 500px) {
     width: 450px;
   }
